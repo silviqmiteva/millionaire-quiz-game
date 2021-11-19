dc2fa165fca5a18ffe18018326179b52c12038cc
silviqmiteva <silviqmiteva_98@mail.bg>
2021-11-19 23:40:54 +0200

    update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Homepage from '../components/homepage/homepage';
 import Mainpage from '../components/mainpage/mainpage';
 
 export default function AppRouter() {
     return(
-        <BrowserRouter>
+        <HashRouter>
         <Switch>
             <Route path="/" exact component={Homepage} />
             <Route path="/mainpage" exact component={Mainpage} />
             {/* <Redirect from="/*" to="/pagenotfound" /> */}
         </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
